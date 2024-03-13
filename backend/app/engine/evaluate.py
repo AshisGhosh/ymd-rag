70467121d33ccc14050ad4f3b3b9620b14386a7f
AshisGhosh <ashisghosh@live.com>
2024-03-13 15:02:57 -0700

    feat: update dir for evaluation IDs

--- backend/app/engine/evaluate.py
+++ backend/app/engine/evaluate.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import pandas as pd
 import numpy as np
 import pickle
+import json
 
 import nest_asyncio
 nest_asyncio.apply()
@@ -32,7 +33,9 @@ from llama_index.core.vector_stores.types import (
 from app.settings import init_settings
 from app.engine import get_index
 
-EVAL_DIR = "data/eval_data"
+EVAL_PARENT_DIR = "data/eval_data"
+EVAL_ID = "default_query_engine"
+EVAL_DIR = f"{EVAL_PARENT_DIR}/{EVAL_ID}"
 
 def cache_object(obj, filename):
     with open(filename, 'wb') as f:
@@ -47,7 +50,7 @@ def load_cached_object(filename):
 
 async def check_for_dataset():
     try:
-        dataset = QueryResponseDataset.from_json(f"{EVAL_DIR}/mistral_eval_qr_dataset.json")
+        dataset = QueryResponseDataset.from_json(f"{EVAL_PARENT_DIR}/mistral_eval_qr_dataset.json")
         print("Dataset already exists")
     except FileNotFoundError:
         print("Generating new dataset")
@@ -77,6 +80,39 @@ def get_pred_responses(eval_qs, query_engine):
         cache_object(pred_responses, f"{EVAL_DIR}/mistral_pred_responses.pkl")
         return pred_responses
 
+def evaluate_responses(eval_qs, responses, ref_response_strs):
+    if load_cached_object(f"{EVAL_DIR}/mistral_eval_results.pkl") is not None:
+        print("Loading cached results")
+        return load_cached_object(f"{EVAL_DIR}/mistral_eval_results.pkl")
+    else:
+        print("Evaluating responses")
+        print("This may take about 15 minutes")
+        evaluator_c = CorrectnessEvaluator()
+        evaluator_s = SemanticSimilarityEvaluator()
+        evaluator_r = RelevancyEvaluator()
+        evaluator_f = FaithfulnessEvaluator()
+
+        evaluator_dict = {
+        "correctness": evaluator_c,
+        "faithfulness": evaluator_f,
+        "relevancy": evaluator_r,
+        "semantic_similarity": evaluator_s,
+        }
+        batch_runner = BatchEvalRunner(evaluator_dict, workers=2, show_progress=True)
+        
+        start_time = time.time()
+        eval_results = batch_runner.evaluate_responses(
+            eval_qs, responses=responses, reference=ref_response_strs
+        )
+        print(f"Evaluation completed in {time.time() - start_time} seconds")
+        results_df = get_results_df(
+            [eval_results],
+            [EVAL_ID],
+            ["correctness", "relevancy", "faithfulness", "semantic_similarity"],
+        )
+        cache_object(results_df, f"{EVAL_DIR}/mistral_eval_results.pkl")
+        return results_df
+
 async def generate_eval_dataset():
     index = get_index()
     print("Index loaded")
@@ -102,10 +138,6 @@ async def generate_eval_dataset():
 
 async def evaluate(eval_dataset):
     print(f"Evaluating dataset with {len(eval_dataset.questions)} questions")
-    evaluator_c = CorrectnessEvaluator()
-    evaluator_s = SemanticSimilarityEvaluator()
-    evaluator_r = RelevancyEvaluator()
-    evaluator_f = FaithfulnessEvaluator()
 
     eval_qs = eval_dataset.questions
     qr_pairs = eval_dataset.qr_pairs
@@ -123,26 +155,15 @@ async def evaluate(eval_dataset):
     # )
 
     pred_response_strs = [str(p) for p in pred_responses]
+    # save as json
+    with open(f"{EVAL_DIR}/mistral_pred_responses.json", "w") as f:
+        json.dump(pred_response_strs, f)
+    
+    eval_results = evaluate_responses(eval_qs, pred_responses, ref_response_strs)
+    print(eval_results)
+
     # base_pred_response_strs = [str(p) for p in base_pred_responses]
-    evaluator_dict = {
-    "correctness": evaluator_c,
-    "faithfulness": evaluator_f,
-    "relevancy": evaluator_r,
-    "semantic_similarity": evaluator_s,
-    }
-    batch_runner = BatchEvalRunner(evaluator_dict, workers=2, show_progress=True)
-    print("Evaluating responses")
-    print("This may take about 15 minutes")
-    start_time = time.time()
-    eval_results = await batch_runner.aevaluate_responses(
-        eval_qs, responses=pred_responses, reference=ref_response_strs
-    )
-    print(f"Responses evaluated in {time.time() - start_time} seconds")
-    results_df = get_results_df(
-        [eval_results],
-        ["Default Query Engine"],
-        ["correctness", "relevancy", "faithfulness", "semantic_similarity"],
-    )
+
     # base_eval_results = await batch_runner.aevaluate_responses(
     #     eval_qs, responses=base_pred_responses, reference=ref_response_strs
     # )
@@ -152,9 +173,7 @@ async def evaluate(eval_dataset):
     #     ["correctness", "relevancy", "faithfulness", "semantic_similarity"],
     # )
 
-    print(results_df)
-    cache_object(results_df, f"{EVAL_DIR}/mistral_eval_results.pkl")
-    results_df.to_csv(f"{EVAL_DIR}/mistral_eval_results.csv")
+    
 
 
 if __name__ == "__main__":
